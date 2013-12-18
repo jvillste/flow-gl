15b57f02b311d62105dd86b5ddc196ff3d5c1e31
Jukka Villstedt <jukka.villstedt@gmail.com>
2013-12-18 23:02:30 +0200

    x

--- src/examples/hello_world.clj
+++ src/examples/hello_world.clj
@@ -2,10 +2,15 @@
   (:require (flow-gl.gui [drawable :as drawable]
                          [layout :as layout]
                          [application :as application]
-                         [view :as view])))
+                         [view :as view])
+            (flow-gl.dataflow [triple-dataflow :as triple-dataflow])))
+
 (defn start []
-   (application/start (view/view [state] (layout/->VerticalStack [(drawable/->Rectangle 100 100 [0 0 1 1])
-                                                                  (drawable/->Rectangle 10 10 [0 1 0 1])])))
+  (application/start (view/view [state]
+                                (let [globals (triple-dataflow/switch-entity state :globals)]
+                                  (layout/->VerticalStack [(drawable/->Rectangle (- (:width globals)
+                                                                                    20) 100 [0 0 1 1])
+                                                           (drawable/->Rectangle 10 10 [0 1 0 1])]))))
   ;;(application/start (view/view [state] (drawable/->Rectangle 100 100 [0 1 1 1])))
   )
 
