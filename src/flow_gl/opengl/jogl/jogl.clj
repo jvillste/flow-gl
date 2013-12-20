cfca83319e03b229d62375b194fa5fb8dd140ac6
Jukka Villstedt <jukka.villstedt@gmail.com>
2013-12-20 21:27:35 +0200

    x

--- src/flow_gl/opengl/jogl/jogl.clj
+++ src/flow_gl/opengl/jogl/jogl.clj
@@ -1,1 +1,28 @@
-(ns flow-gl.opengl.jogl.jogl)
+(ns flow-gl.opengl.jogl.jogl
+  (:import [com.jogamp.newt.event WindowAdapter WindowEvent]
+           [com.jogamp.newt.opengl GLWindow]
+           [javax.media.opengl GLCapabilities GLProfile]))
+
+
+(let [gl-profile (GLProfile/getDefault)
+      gl-capabilities (GLCapabilities. gl-profile)
+      window (GLWindow/create gl-capabilities)]
+  (.setSize window 300 300)
+  (.setVisible window true)
+  (.addWindowListener window (proxy [WindowAdapter] []
+                        (windowDestroyNotify [e]
+                          (println "close")))))
+
+;; GLProfile glp = GLProfile.getDefault();
+;;        GLCapabilities caps = new GLCapabilities(glp);
+
+;;        GLWindow window = GLWindow.create(caps);
+;;        window.setSize(300, 300);
+;;        window.setVisible(true);
+;;        window.setTitle("NEWT Window Test");
+
+;;        window.addWindowListener(new WindowAdapter() {
+;;            public void windowDestroyNotify(WindowEvent arg0) {
+;;                System.exit(0);
+;;            };
+;;        });
