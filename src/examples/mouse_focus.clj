d0fe54e94a7205b05710ab6b67be15a295a2e83b
Jukka Villstedt <jukka.villstedt@gmail.com>
2014-03-10 22:23:42 +0200

    x

--- src/examples/mouse_focus.clj
+++ src/examples/mouse_focus.clj
@@ -18,9 +18,30 @@
         flow-gl.gui.layout-dsl))
 
 
+(defn child-path-to-state-path [root-layout child-path]
+  (loop [state-path []
+         rest-of-child-path child-path
+         child-path []]
+    (if-let [child-path-part (first rest-of-child-path)]
+      (let [child-path (conj child-path child-path-part)]
+        (if-let [state-path-part (get-in root-layout (conj child-path :state-path))]
+          (recur (concat state-path state-path-part)
+                 (rest rest-of-child-path)
+                 child-path)
+          (recur state-path
+                 (rest rest-of-child-path)
+                 child-path)))
+      state-path)))
+
+(fact (child-path-to-state-path {:children [{:state-path [:foo 1]
+                                             :child {:child {:state-path [:bar]
+                                                             :child {}}}}]}
+                                [:children 0 :child :child :child])
+      => nil)
+
 (defn apply-mouse-event-handlers [state root-layout child-paths event]
   (reduce (fn [state child-path]
-            ((get-in root-layout (conj child-path :handle-mouse-event))  state event))
+            (update-in state (child-path-to-state-path root-layout child-path) (get-in root-layout (conj child-path :handle-mouse-event)) event))
           state
           (filter (fn [child-path]
                     (:handle-mouse-event (get-in root-layout child-path)))
@@ -102,23 +123,33 @@
         (window/close window)
         (throw e)))))
 
-
-(defn click-counter-view [state state-path]
-  (assoc (layout/->Box 10 [(drawable/->Rectangle 0
-                                                 0
-                                                 [0 0.5 0.5 1])
-                           (drawable/->Text (str (:count state))
-                                            (font/create "LiberationSans-Regular.ttf" 15)
-                                            [0 0 0 1])])
+(defn on-mouse-clicked [layoutable handler]
+  (assoc layoutable
     :handle-mouse-event (fn [state event]
                           (if (= :mouse-clicked (:type event))
-                            (update-in state (conj state-path :count) inc)
+                            (handler state)
                             state))))
 
+(defn click-counter-view [state]
+  (-> (layout/->Box 10 [(drawable/->Rectangle 0
+                                              0
+                                              [0 0.5 0.5 1])
+                        (-> (drawable/->Text (str (:count state))
+                                             (font/create "LiberationSans-Regular.ttf" 15)
+                                             [0 0 0 1])
+
+                            (on-mouse-clicked #(update-in % [:count] (partial + 2))))])
+
+      (on-mouse-clicked #(update-in % [:count] inc))))
+
+(defn call-view-for-sequence [parent-state view-function key]
+  (map-indexed (fn [index child-state]
+                 (assoc (view-function child-state)
+                   :state-path [key index]))
+               (key parent-state)))
+
 (defn view [state]
-  (layout/->VerticalStack (map-indexed (fn [index counter]
-                                         (click-counter-view counter [:counters index]))
-                                       (:counters state))))
+  (layout/->VerticalStack (call-view-for-sequence state click-counter-view :counters)))
 
 
 (defn handle-event [state event]
@@ -133,11 +164,6 @@
   (.start (Thread. (fn [] (start-view #'view
                                       @event-queue
                                       #'handle-event
-                                      {:counters [{:count 0}{:count 0}{:count 0}]}))))
-
-  #_(start-view view
-                @event-queue
-                handle-event
-                (model-to-view-state log initial-view-state)))
+                                      {:counters [{:count 0}{:count 0}{:count 0}]})))))
 
 (event-queue/add-event @event-queue {})
