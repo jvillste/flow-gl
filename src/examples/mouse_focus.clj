9a94d41f6a8ada2958f2721c52f19059ba07d322
Jukka Villstedt <jukka.villstedt@gmail.com>
2014-03-19 21:27:15 +0200

    Closin with esc works in mouse-focus

--- src/examples/mouse_focus.clj
+++ src/examples/mouse_focus.clj
@@ -29,7 +29,9 @@
       prefixes)))
 
 (fact (path-prefixes [[1 2] [3] [4]])
-      => [[[1 2]] [[1 2] [3]] [[1 2] [3] [4]]])
+      => [[[1 2]]
+          [[1 2] [3]]
+          [[1 2] [3] [4]]])
 
 (defn layout-path-to-state-path [root-layout child-path]
   (loop [state-path []
@@ -88,13 +90,16 @@
   (reduce (fn [state focus-path-parts]
             (let [focus-path (apply concat focus-path-parts)]
               (if-let [keyboard-event-handler (get-in state (conj (vec focus-path) :handle-keyboard-event))]
-                (update-in state
-                           focus-path
-                           keyboard-event-handler
-                           event)
+                (if (seq focus-path)
+                  (update-in state
+                             focus-path
+                             keyboard-event-handler
+                             event)
+                  (keyboard-event-handler state event))
                 state)))
           state
-          (path-prefixes focus-path-parts)))
+          (conj (path-prefixes focus-path-parts)
+                [])))
 
 (defn set-focus-state [state focus-path-parts has-focus]
   (if (seq focus-path-parts)
@@ -170,7 +175,7 @@
         (next-focus-path-parts state [[:children1 1] [:children2 1]])  => [[:children1 1] [:children2 2] [:children3 0]]
         (next-focus-path-parts state [[:children1 1] [:children2 2] [:children3 2]])  => nil))
 
-(defn start-view [view event-queue event-handler initial-state]
+(defn start-view [view event-queue initial-state]
   (let [window (window/create 300
                               400
                               opengl/initialize
@@ -185,90 +190,93 @@
                previous-focus-path-parts initial-focus-path-parts-value
                cached-runnables {}]
 
-          (let [layoutable (view state)
-                layout (layout/layout layoutable
-                                      (window/width window)
-                                      (window/height window))
-                drawing-commands (doall (drawable/drawing-commands layout))
-                cached-runnables-atom (atom cached-runnables)
-                unused-commands-atom (atom nil)]
+          (if (:close-requested state)
+            (do (println "closing")
+                (window/close window))
 
-            (when (not= state previous-state)
-              (window/render window gl
-                             (opengl/clear gl 0 0 0 1)
-                             (doseq [runnable (doall (reduce (fn [runnables command]
-                                                               (if (contains? @cached-runnables-atom command)
-                                                                 (conj runnables (get @cached-runnables-atom command))
-                                                                 (let [runnable (command/create-runner command gl)]
-                                                                   (swap! cached-runnables-atom assoc command runnable)
-                                                                   (conj runnables runnable))))
-                                                             []
-                                                             drawing-commands))]
+            (let [layoutable (view state)
+                  layout (layout/layout layoutable
+                                        (window/width window)
+                                        (window/height window))
+                  drawing-commands (doall (drawable/drawing-commands layout))
+                  cached-runnables-atom (atom cached-runnables)
+                  unused-commands-atom (atom nil)]
 
-                               ;;(println (type runnable))
-                               (command/run runnable gl))
+              (when (not= state previous-state)
+                (window/render window gl
+                               (opengl/clear gl 0 0 0 1)
+                               (doseq [runnable (doall (reduce (fn [runnables command]
+                                                                 (if (contains? @cached-runnables-atom command)
+                                                                   (conj runnables (get @cached-runnables-atom command))
+                                                                   (let [runnable (command/create-runner command gl)]
+                                                                     (swap! cached-runnables-atom assoc command runnable)
+                                                                     (conj runnables runnable))))
+                                                               []
+                                                               drawing-commands))]
 
-                             (let [unused-commands (filter (complement (apply hash-set drawing-commands))
-                                                           (keys @cached-runnables-atom))]
-                               (doseq [unused-command unused-commands]
-                                 (if-let [unused-runnable (get unused-command @cached-runnables-atom)]
-                                   (command/delete unused-runnable gl)))
+                                 ;;(println (type runnable))
+                                 (command/run runnable gl))
 
-                               (reset! unused-commands-atom unused-commands))))
+                               (let [unused-commands (filter (complement (apply hash-set drawing-commands))
+                                                             (keys @cached-runnables-atom))]
+                                 (doseq [unused-command unused-commands]
+                                   (if-let [unused-runnable (get unused-command @cached-runnables-atom)]
+                                     (command/delete unused-runnable gl)))
 
-            (let [event (event-queue/dequeue-event-or-wait event-queue)
-                  live-commands (apply dissoc @cached-runnables-atom @unused-commands-atom)]
-              (cond
-               (= (:source event)
-                  :mouse)
-               (let [layout-paths-under-mouse (layout/layout-paths-in-coordinates layout (:x event) (:y event))
-                     layout-path-under-mouse (last layout-paths-under-mouse)]
+                                 (reset! unused-commands-atom unused-commands))))
 
-                 (if (= (:type event)
-                        :mouse-clicked)
-                   (let [focus-path-parts (layout-path-to-state-path-parts layout layout-path-under-mouse)]
+              (let [event (event-queue/dequeue-event-or-wait event-queue)
+                    live-commands (apply dissoc @cached-runnables-atom @unused-commands-atom)]
+                (cond
+                 (= (:source event)
+                    :mouse)
+                 (let [layout-paths-under-mouse (layout/layout-paths-in-coordinates layout (:x event) (:y event))
+                       layout-path-under-mouse (last layout-paths-under-mouse)]
 
-                     (recur (-> state
-                                (set-focus-state previous-focus-path-parts false)
-                                (set-focus-state focus-path-parts true)
-                                (apply-mouse-event-handlers layout
-                                                            layout-path-under-mouse
-                                                            event))
+                   (if (= (:type event)
+                          :mouse-clicked)
+                     (let [focus-path-parts (layout-path-to-state-path-parts layout layout-path-under-mouse)]
+
+                       (recur (-> state
+                                  (set-focus-state previous-focus-path-parts false)
+                                  (set-focus-state focus-path-parts true)
+                                  (apply-mouse-event-handlers layout
+                                                              layout-path-under-mouse
+                                                              event))
+                              state
+                              focus-path-parts
+                              live-commands))
+
+                     (recur (apply-mouse-event-handlers state
+                                                        layout
+                                                        layout-path-under-mouse
+                                                        event)
                             state
-                            focus-path-parts
-                            live-commands))
+                            previous-focus-path-parts
+                            live-commands)))
 
-                   (recur (apply-mouse-event-handlers state
-                                                      layout
-                                                      layout-path-under-mouse
-                                                      event)
+                 (events/key-pressed? event :tab)
+                 (let [next-focus-path-parts-value (or (next-focus-path-parts state previous-focus-path-parts)
+                                                       (initial-focus-path-parts state))]
+                   (recur (-> state
+                              (set-focus-state previous-focus-path-parts false)
+                              (set-focus-state next-focus-path-parts-value true))
                           state
-                          previous-focus-path-parts
-                          live-commands)))
+                          next-focus-path-parts-value
+                          live-commands))
 
-               (events/key-pressed? event :tab)
-               (let [next-focus-path-parts-value (or (next-focus-path-parts state previous-focus-path-parts)
-                                                     (initial-focus-path-parts state))]
-                 (recur (-> state
-                            (set-focus-state previous-focus-path-parts false)
-                            (set-focus-state next-focus-path-parts-value true))
+                 (= (:type event)
+                    :close-requested)
+                 (do (println "closing")
+                     (window/close window))
+
+                 :default
+                 (recur (apply-keyboard-event-handlers state
+                                                       previous-focus-path-parts
+                                                       event)
                         state
-                        next-focus-path-parts-value
-                        live-commands))
-
-               (= (:type event)
-                  :close-requested)
-               (do (println "closing")
-                   (window/close window))
-
-               :default
-               (recur #_(event-handler state event)
-                      (apply-keyboard-event-handlers state
-                                                     previous-focus-path-parts
-                                                     event)
-                      state
-                      previous-focus-path-parts
-                      live-commands))))))
+                        previous-focus-path-parts
+                        live-commands)))))))
 
 
       (catch Exception e
@@ -281,15 +289,6 @@
                           (if (= :mouse-clicked (:type event))
                             (handler state)
                             state))))
-
-(defn handle-click-counter-keyboard-event [state event]
-  (cond (events/key-pressed? event :enter)
-        (-> (update-in state [:count] inc))
-
-        :default
-        state))
-
-
 (defn seq-focus-handlers [child-seq-key]
   {:first-focusable-child [child-seq-key 0]
    :next-focusable-child (fn [this [_ child-index]]
@@ -298,6 +297,21 @@
                                     (count (child-seq-key this)))
                                nil
                                [child-seq-key (inc child-index)])))})
+
+
+(defn handle-click-counter-keyboard-event [state event]
+  (cond (events/key-pressed? event :enter)
+        (-> (update-in state [:count] inc))
+
+        :default
+        state))
+
+(defn call-view-for-sequence [parent-state view-function key]
+  (map-indexed (fn [index child-state]
+                 (assoc (view-function child-state)
+                   :state-path-part [key index]))
+               (key parent-state)))
+
 
 (defn click-counter-view [state]
   (-> (layout/->Box 10 [(drawable/->Rectangle 0
@@ -311,49 +325,45 @@
 
       (on-mouse-clicked #(update-in % [:count] inc))))
 
-
-
-(defn create-click-counter []
+(defn click-counter []
   {:count 0
    :handle-keyboard-event handle-click-counter-keyboard-event})
-
-(defn call-view-for-sequence [parent-state view-function key]
-  (map-indexed (fn [index child-state]
-                 (assoc (view-function child-state)
-                   :state-path-part [key index]))
-               (key parent-state)))
 
 (defn counter-row-view [state]
   (layout/->Margin 5 0 5 0
                    [(layout/->Box 10 [(drawable/->FilledRoundedRectangle 0 0 10 (if (:child-has-focus state)
                                                                                   [0.8 0.8 0.8 1]
                                                                                   [0.5 0.5 0.5 1]))
-                                      (layout/->HorizontalStack (call-view-for-sequence state click-counter-view :counters))])]))
+                                      (layout/->HorizontalStack (call-view-for-sequence state
+                                                                                        click-counter-view
+                                                                                        :counters))])]))
 
-(defn create-counter-row [& counters]
+(defn counter-row [& counters]
   (-> {:counters (vec counters)}
       (conj (seq-focus-handlers :counters))))
 
-(defn view [state]
-  (layout/->VerticalStack (call-view-for-sequence state counter-row-view :counter-rows)))
+(defn counters-view [state]
+  (layout/->VerticalStack (call-view-for-sequence state
+                                                  counter-row-view
+                                                  :counter-rows)))
 
-(defn create-view [& counter-rows]
-  (-> {:counter-rows (vec counter-rows)}
+(defn counters [& counter-rows]
+  (-> {:counter-rows (vec counter-rows)
+       :handle-keyboard-event (fn [state event]
+                                (cond (events/key-pressed? event :esc)
+                                      (assoc state :close-requested true)
+
+                                      :default
+                                      state))}
       (conj (seq-focus-handlers :counter-rows))))
-
-(defn handle-event [state event]
-  (cond
-   :default
-   state))
 
 (defonce event-queue (atom (event-queue/create)))
 
 (defn start []
   (reset! event-queue (event-queue/create))
-  (.start (Thread. (fn [] (start-view #'view
+  (.start (Thread. (fn [] (start-view #'counters-view
                                       @event-queue
-                                      #'handle-event
-                                      (create-view (create-counter-row (create-click-counter) (create-click-counter))
-                                                   (create-counter-row (create-click-counter) (create-click-counter))))))))
+                                      (counters (counter-row (click-counter) (click-counter))
+                                                (counter-row (click-counter) (click-counter))))))))
 
 (event-queue/add-event @event-queue {})
