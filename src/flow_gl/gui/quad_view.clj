a3c7d16d9b63ed4bffd87ec6820e96dd0ddfebb1
Jukka Villstedt <jukka.villstedt@gmail.com>
2014-06-19 15:54:37 +0300

    added quad_view.clj

---
+++ src/flow_gl/gui/quad_view.clj
@@ -0,0 +1,167 @@
+(ns flow-gl.gui.quad-view
+  (:require (flow-gl.gui [event-queue :as event-queue]
+                         [layout :as layout]
+                         [drawable :as drawable]
+                         [layoutable :as layoutable])
+            (flow-gl.opengl.jogl [opengl :as opengl]
+                                 [window :as window]
+                                 [triangle-list :as triangle-list]
+                                 [textured-quad :as textured-quad]
+                                 [texture :as texture]
+                                 [shader :as shader]
+                                 [buffer :as buffer]
+                                 [quad-batch :as quad-batch])
+            [flow-gl.opengl.math :as math]
+            (flow-gl.graphics [buffered-image :as buffered-image]
+                              [font :as font]
+                              [text :as text]
+                              [native-buffer :as native-buffer]))
+  (:import [javax.media.opengl GL2]
+           [java.io PrintWriter StringWriter]
+           [java.nio IntBuffer]
+           [java.awt Color])
+
+  (:use clojure.test))
+
+(defn quads-for-layout
+  ([layout]
+     (quads-for-layout layout -1 0))
+
+  ([layout parent next-free-id]
+     (if (satisfies? drawable/Java2DDrawable layout)
+       [{:drawable (dissoc layout :x :y)
+         :x (:x layout)
+         :y (:y layout)
+         :parent parent}]
+       (let [curretn-id next-free-id]
+         (loop [quads [{:x (:x layout)
+                        :y (:y layout)
+                        :parent parent}]
+                children (:children layout)
+                next-free-id (inc next-free-id)]
+           (if-let [child (first children)]
+             (let [child-quads (quads-for-layout child curretn-id next-free-id)]
+               (recur (concat quads
+                              child-quads)
+                      (rest children)
+                      (+ next-free-id (count child-quads))))
+             quads))))))
+
+(deftest quads-for-layout-test
+  (is (=  (let [layout (layout/layout (layout/->HorizontalStack [(assoc (layout/->VerticalStack [(drawable/->Text "Foo"
+                                                                                                                  (font/create "LiberationSans-Regular.ttf" 14)
+                                                                                                                  [1 1 1 1])
+                                                                                                 (assoc (drawable/->Text "Bar"
+                                                                                                                         (font/create "LiberationSans-Regular.ttf" 14)
+                                                                                                                         [1 1 1 1])
+                                                                                                   :state-path-part [:bar] )])
+                                                                   :state-path-part [:child-states 0])
+                                                                 (assoc (layout/->VerticalStack [(drawable/->Text "Foo"
+                                                                                                                  (font/create "LiberationSans-Regular.ttf" 14)
+                                                                                                                  [1 1 1 1])
+                                                                                                 (drawable/->Text "Bar"
+                                                                                                                  (font/create "LiberationSans-Regular.ttf" 14)
+                                                                                                                  [1 1 1 1])])
+                                                                   :state-path-part [:child-states 1])] )
+                                      100 100)]
+            (quads-for-layout (assoc layout :x 0 :y 0)
+                              -1
+                              0))
+
+          nil)))
+
+(defn unused-drawable-textures [drawable-textures quads]
+  (->> quads
+       (map :drawable)
+       (reduce dissoc drawable-textures)))
+
+(defn new-drawables [drawable-textures quads]
+  (reduce (fn [new-drawables quad]
+            (if (contains? quad :drawable)
+              (if (contains? drawable-textures (:drawable quad))
+                new-drawables
+                (conj new-drawables (:drawable quad)))
+              new-drawables ))
+          #{}
+          quads))
+
+(defn create-textures [drawables]
+  (map (fn [drawable]
+         (let [buffered-image (buffered-image/create (max 1 (:width drawable))
+                                                     (max 1 (:height drawable)))]
+           (drawable/draw drawable
+                          (buffered-image/get-graphics buffered-image))
+           buffered-image))
+       drawables))
+
+(defn add-new-textures [drawable-textures drawables first-texture-id]
+  (loop [texture-id first-texture-id
+         drawable-textures drawable-textures
+         drawables drawables]
+    (if-let [drawable (first drawables)]
+      (recur (inc texture-id)
+             (assoc drawable-textures drawable texture-id)
+             (rest drawables))
+      drawable-textures)))
+
+(defn load-new-textures [quad-view quads gl]
+  (let [first-texture-id (:next-free-texture-id (:quad-batch quad-view))
+        drawables (new-drawables (:drawable-textures quad-view) quads)
+        new-textures (create-textures drawables)]
+    (if (empty? new-textures)
+      quad-view
+      (assoc quad-view
+        :quad-batch (quad-batch/add-textures (:quad-batch quad-view) gl new-textures)
+        :drawable-textures (add-new-textures (:drawable-textures quad-view) drawables (:next-free-texture-id (:quad-batch quad-view)))))))
+
+(defn add-texture-ids [quads drawable-textures]
+  (map (fn [quad]
+         (if (contains? quad :drawable)
+           (assoc quad
+             :texture-id (get drawable-textures
+                              (:drawable quad)))
+
+           quad))
+       quads))
+
+(defn unload-unused-textures [quad-view quads]
+  (let [unused-drawable-textures (unused-drawable-textures (:drawable-textures quad-view)
+                                                           quads)
+        new-quad-batch (reduce quad-batch/remove-texture
+                               (:quad-batch quad-view)
+                               (vals unused-drawable-textures))
+        new-drawable-textures (apply dissoc
+                                     (:drawable-textures quad-view)
+                                     (keys unused-drawable-textures))]
+
+    (assoc quad-view
+      :quad-batch new-quad-batch
+      :drawable-textures new-drawable-textures)))
+
+(defn draw-quads [quad-view quads width height gl]
+  #_(println (select-keys (:quad-batch quad-view)
+                          [:allocated-texels
+                           :next-free-texel
+                           :removed-texels]))
+  (let [quad-view (load-new-textures quad-view
+                                     quads
+                                     gl)
+        quad-view (unload-unused-textures quad-view quads)
+        quads (add-texture-ids quads
+                               (:drawable-textures quad-view))]
+    (assoc quad-view :quad-batch
+           (quad-batch/draw-quads (:quad-batch quad-view)
+                                  gl
+                                  quads
+                                  width height))))
+
+(defn draw-layout [quad-view layout width height gl]
+  (draw-quads quad-view
+              (quads-for-layout layout)
+              width
+              height
+              gl))
+
+(defn create [gl]
+  {:drawable-textures {}
+   :quad-batch (quad-batch/create gl)})
