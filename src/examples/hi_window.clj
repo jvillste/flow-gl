e4eeddde7c7edb34d12d2e2b6d507d6f341de676
Jukka Villstedt <jukka.villstedt@nitor.fi>
2016-09-13 08:44:19 +0300

    simplify hi_window

--- src/examples/hi_window.clj
+++ src/examples/hi_window.clj
@@ -4,25 +4,22 @@
             [flow-gl.gui.window :as window]
             [clojure.core.async :as async]))
 
-(defn start []
-  (let [window (jogl-window/create 200
-                                   200)]
+(let [window (jogl-window/create 200
+                                 200)
+      event-channel (window/event-channel window)]
 
-    (loop [red true]
-      (window/with-gl window gl
-        (if red
-          (opengl/clear gl 1 0 0 1)
-          (opengl/clear gl 0 0 1 1)))
+  (loop [red true]
+    (window/with-gl window gl
+      (if red
+        (opengl/clear gl 1 0 0 1)
+        (opengl/clear gl 0 0 1 1)))
+    
+    (window/swap-buffers window)
+    
+    (let [event (async/<!! event-channel)]
+      (prn event)
       
-      (window/swap-buffers window)
-      
-      (let [event (async/<!! (window/event-channel window))]
-        (println event)
-        
-        (if (or (= (:key event) :esc)
-                (= (:type event) :close-requested))
-          (window/close window)
-          (recur (not red)))))))
-
-
-
+      (if (or (= (:key event) :esc)
+              (= (:type event) :close-requested))
+        (window/close window)
+        (recur (not red))))))
