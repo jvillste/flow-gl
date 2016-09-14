03d4adaedeaa97d9b46a0f8dd04bfc76cb0066ce
Jukka Villstedt <jukka.villstedt@nitor.fi>
2016-09-14 08:53:08 +0300

    bump up priority-map version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [net.mikera/core.matrix "0.33.2"]
                  [com.taoensso/timbre "3.3.1"]
-                 [org.clojure/data.priority-map "0.0.2"]
+                 [org.clojure/data.priority-map "0.0.7"]
                  [org.clojure/core.async "0.2.391"]
                  [clj-http "2.2.0"]
                  [org.clojars.jvillste/jogl-all "2.1.5"]])
