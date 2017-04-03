f0163064621d9448c1d1b1d4397eaaef08e434f3
Jukka Villstedt <jukka.villstedt@nitor.fi>
2017-04-03 17:38:18 +0300

    refactor paint.clj

--- src/examples/paint.clj
+++ src/examples/paint.clj
@@ -5,7 +5,8 @@
                    [application :as application]
                    [layouts :as layouts]
                    [layout :as layout]
-                   [cache :as cache])
+                   [cache :as cache]
+                   [handler :as handler])
             (flow-gl.gui 
              
              [visuals :as visuals]
@@ -17,7 +18,8 @@
                                  [quad :as quad]
                                  [texture :as texture]
                                  [render-target :as render-target])
-            [flow-gl.graphics.buffered-image :as buffered-image]))
+            [flow-gl.graphics.buffered-image :as buffered-image])
+  (:use clojure.test))
 
 (def fragment-shader-source "
   #version 140
@@ -47,15 +49,13 @@
   void main() {
     float distance = 1;
     for (int i = 0; i < (number_of_points -1); i++){
-      distance = min(distance, distance_to_line(texture_coordinate, points[i], points[i+1], 0.1));
+      distance = min(distance, distance_to_line(texture_coordinate, points[i], points[i+1], 0.1) / 0.01);
     }
+    distance = min(1,distance);
 
-    // outColor = vec4(0,0,0, max(texture(texture, texture_coordinate), 1.0 - distance_to_line(texture_coordinate, points[0], points[1], 0.1)/ 0.01));
+     vec2 flipped_texture_coordinate = vec2(texture_coordinate.x, 1.0 - texture_coordinate.y);
+     outColor = vec4(0,0,0, max(texture(texture, flipped_texture_coordinate).a,  1.0 - distance));
 
-vec2 flipped_texture_coordinate = vec2(texture_coordinate.x, 1.0 - texture_coordinate.y);
-     outColor = vec4(0,0,0, max(texture(texture, flipped_texture_coordinate).a, 1.0 - distance / 0.01));
-   //  outColor = vec4(0,0,0, max(texture(texture, texture_coordinate).a, 1.0 - distance / 0.01));
-//     outColor = vec4(0,0,0, 1.0 - distance / 0.01);
 
     //outColor = vec4(0,0,0, distance); //max( 0.0, 1.0 - distance));
   }
@@ -132,10 +132,168 @@ vec2 flipped_texture_coordinate = vec2(texture_coordinate.x, 1.0 - texture_coord
 
 (def target-buffered-image (buffered-image/create-from-file "pumpkin.png"))
 
+(defn create-events-set [& events]
+  (apply sorted-set-by (fn [a b]
+                         (compare (:time a)
+                                  (:time b)))
+         events))
+
+(defn partition-on [predicate xs]
+  (loop [xs xs
+         partitions []
+         partition []]
+    (if-let [x (first xs)]
+      (if (predicate x)
+        (recur (rest xs)
+               (if (empty? partition)
+                 partitions
+                 (conj partitions partition))
+               [x])
+        (recur (rest xs)
+               partitions
+               (conj partition x)))
+      (if (empty? partition)
+        partitions
+        (conj partitions partition)))))
+
+(deftest partition-on-test
+  (is (= [[1 1] [2 1 1 1] [2]]
+         (partition-on even? [1 1 2 1 1 1 2])))
+
+  (is (= [[2 1] [2 1 1] [2]]
+         (partition-on even? [2 1 2 1 1 2])))
+
+  (is (= []
+         (partition-on even? []))))
+
+(defn strokes [events last-painted-event]
+  (->> (if last-painted-event
+         (subseq events >= last-painted-event)
+         events)
+       (partition-on (fn [event]
+                       (= :start-stroke
+                          (:type event))))))
+
+(deftest strokes-test
+  (let [stroke-1 [{:time 1 :type :start-stroke}
+                  {:time 2 :type :draw-stroke}
+                  {:time 3 :type :draw-stroke}
+                  {:time 4 :type :end-stroke}]
+        stroke-2 [{:time 10 :type :start-stroke}
+                  {:time 11 :type :draw-stroke}
+                  {:time 12 :type :end-stroke}]]
+    (is (= [[{:time 1, :type :start-stroke}
+             {:time 2, :type :draw-stroke}
+             {:time 3, :type :draw-stroke}
+             {:time 4, :type :end-stroke}]]
+           (strokes (apply create-events-set stroke-1)
+                    nil)))
+
+    (is (= [stroke-1
+            stroke-2]
+           (strokes (apply create-events-set (concat stroke-1
+                                                     stroke-2))
+                    nil)))
+
+    (is (= [[{:time 3, :type :draw-stroke}
+             {:time 4, :type :end-stroke}]]
+           (strokes (apply create-events-set stroke-1)
+                    {:time 3 :type :draw-stroke})))
+
+    (is (= [[{:time 3 :type :draw-stroke}]]
+           (strokes (create-events-set {:time 3, :type :draw-stroke})
+                    {:time 3 :type :draw-stroke})))
+
+    (is (= []
+           (strokes (create-events-set)
+                    {:time 3 :type :draw-stroke})))))
+
+
+(defn draw-stroke [events canvas-state-atom gl]
+  (let [width (:width (:target @canvas-state-atom))
+        height (:height (:target @canvas-state-atom))
+        coordinates (apply concat (map (fn [event]
+                                         [(float (/ (:x event)
+                                                    width))
+                                          (float (/ (:y event)
+                                                    height))])
+                                       events))]
+    (prn coordinates)
+    (render-target/render-to (:target @canvas-state-atom) gl
+                             (let [program (cache/call-with-key! quad/create-program
+                                                                 fragment-shader-source
+                                                                 fragment-shader-source
+                                                                 gl)]
+                               (quad/draw gl
+                                          ["texture" (:texture (:source @canvas-state-atom))]
+                                          [:2fv "points" coordinates
+                                           :1i "number_of_points" (/ (count coordinates)
+                                                                     2)]
+                                          program
+                                          0 0
+                                          width
+                                          height
+                                          width
+                                          height)))
+
+    (swap! canvas-state-atom (fn [state]
+                               (assoc state
+                                      :source (:target state)
+                                      :target (:source state))))))
+
+(handler/def-handler-creator create-canvas-renderer [events] [scene-graph gl]
+  (let [canvas-state-atom (atom-registry/get! [:canvas-state
+                                               (:width scene-graph)
+                                               (:height scene-graph)]
+                                              (canvas-state-atom-specification gl
+                                                                               (:width scene-graph)
+                                                                               (:height scene-graph)))
+        strokes (strokes events (:last-painted-event @canvas-state-atom))]
+    
+    (doseq [stroke strokes]
+      (when (not= [(:last-painted-event @canvas-state-atom)]
+                       stroke)
+        
+        (draw-stroke (if (= 1 (count stroke))
+                       [(first stroke)
+                        (first stroke)]
+                       stroke)
+                     canvas-state-atom
+                     gl)))
+
+    (swap! canvas-state-atom assoc :last-painted-event (last (last strokes)))
+
+    (assoc (select-keys scene-graph [:x :y :width :height])
+           :texture-id (:texture (:source  @canvas-state-atom))
+           :texture-hash (hash scene-graph))))
+
+(handler/def-handler-creator create-canvas-mouse-event-handler [event-state-atom] [node event]
+  (when-let [paint-event (case (:type event)
+                           :mouse-pressed {:type :start-stroke
+                                           :x (:local-x event)
+                                           :y (:local-y event)
+                                           :time (:time event)}
+
+                           :mouse-dragged {:type :draw-stroke
+                                           :x (:local-x event)
+                                           :y (:local-y event)
+                                           :time (:time event)}
+
+                           :mouse-released {:type :end-stroke
+                                            :x (:local-x event)
+                                            :y (:local-y event)
+                                            :time (:time event)}
+
+                           nil)]
+    (swap! event-state-atom update :events conj paint-event))
+  event)
+
+
+
 (defn create-scene-graph [width height]
   (let [canvas-width 500
         canvas-height 500
-        event-state-atom (atom-registry/get! :state {:create (fn [] {:points []})})]
+        event-state-atom (atom-registry/get! :state {:create (fn [] {:events (create-events-set)})})]
     (animation/swap-state! animation/set-wake-up 1000)
     (-> {:x 0
          :y 0
@@ -154,56 +312,8 @@ vec2 flipped_texture_coordinate = vec2(texture_coordinate.x, 1.0 - texture_coord
                                                        :width canvas-width
                                                        :height canvas-height
                                                        :id :canvas
-                                                       :mouse-event-handler (fn [node event]
-                                                                              (when (= (:type event)
-                                                                                       :mouse-dragged)
-                                                                                (swap! event-state-atom update :points conj [(:local-x event)
-                                                                                                                             (:local-y event)]))
-                                                                              event)
-                                                       :render (fn [scene-graph gl]
-                                                                 (let [canvas-state-atom (atom-registry/get! [:canvas-state canvas-width canvas-height] (canvas-state-atom-specification gl canvas-width canvas-height))
-                                                                       points (:points (first (swap-and-return-old-and-new! event-state-atom assoc :points [])))
-                                                                       points (if (= 1 (count points))
-                                                                                (concat points points)
-                                                                                points)
-                                                                       points (map (fn [[x y]]
-                                                                                     [(float (/ x canvas-width))
-                                                                                      (float (/ y canvas-height))])
-                                                                                   points)
-                                                                       coordinates (flatten points)
-                                                                       paint (> (count coordinates)
-                                                                                0)]
-                                                                   (when paint
-                                                                     (prn coordinates (/ (count coordinates)
-                                                                                         2))
-                                                                     (render-target/render-to (:target @canvas-state-atom) gl
-                                                                                              #_(opengl/clear gl 1 1 1 1)
-                                                                                              (let [program (cache/call-with-key! quad/create-program
-                                                                                                                                  fragment-shader-source
-                                                                                                                                  fragment-shader-source
-                                                                                                                                  gl)]
-                                                                                                (quad/draw gl
-                                                                                                           ["texture" (:texture (:source @canvas-state-atom))]
-                                                                                                           [:2fv "points" coordinates
-                                                                                                            :1i "number_of_points" (/ (count coordinates)
-                                                                                                                                      2)]
-                                                                                                           program
-                                                                                                           0 0
-                                                                                                           canvas-width
-                                                                                                           canvas-height
-                                                                                                           canvas-width
-                                                                                                           canvas-height)))
-
-                                                                     (swap! canvas-state-atom (fn [state]
-                                                                                                (assoc state
-                                                                                                       :source (:target state)
-                                                                                                       :target (:source state)))))
-
-                                                                   (assoc (select-keys scene-graph [:x :y])
-                                                                          :width canvas-width
-                                                                          :height canvas-height
-                                                                          :texture-id (:texture (:source  @canvas-state-atom))
-                                                                          :texture-hash (hash scene-graph))))})]}
+                                                       :mouse-event-handler (create-canvas-mouse-event-handler event-state-atom)
+                                                       :render (create-canvas-renderer (:events @event-state-atom))})]}
         (application/do-layout width height))))
 
 (defn start []
