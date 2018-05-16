6c0141e2d572e21820c52e5b0890683a7671ce13
Jukka Villstedt <jukka.villstedt@iki.fi>
2018-05-16 08:27:34 +0300

    add comment to root-renderer

--- src/fungl/root_renderer.clj
+++ src/fungl/root_renderer.clj
@@ -3,6 +3,8 @@
             [flow-gl.gui.quad-renderer :as quad-renderer]
             [fungl.atom-registry :as atom-registry]))
 
+
+;; This was in application.clj but resulted to a cyclic dependency between application and atom-registry
 (defn root-renderer [scene-graph gl]
   (opengl/clear gl 0 0 0 1)
   (let [quad-renderer-atom (atom-registry/get! ::root-renderer (quad-renderer/atom-specification gl))]
