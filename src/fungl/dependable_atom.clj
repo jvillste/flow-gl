c69d76d62f11c5bf1fec583ccd7cbe5d99798fd7
Jukka Villstedt <jukka.villstedt@iki.fi>
2020-02-25 14:46:46 +0200

    add dependable atom

---
+++ src/fungl/dependable_atom.clj
@@ -0,0 +1,38 @@
+(ns fungl.dependable-atom
+  (:require [fungl.depend :as depend])
+  (:refer-clojure :exclude [atom]))
+
+(defrecord Atom [atom]
+  clojure.lang.IDeref
+  (deref [this]
+    (depend/add-dependency {:type ::type
+                            :atom atom}
+                           @atom)
+    (deref atom))
+
+  clojure.lang.IAtom
+  (swap [this fn]
+    (swap! atom fn))
+
+  (swap [this fn arg]
+    (swap! atom fn arg))
+
+  (swap [this fn arg1 arg2]
+    (swap! atom fn arg1 arg2))
+
+  (swap [this fn arg1 arg2 args]
+    (apply swap! atom fn arg1 arg2 args))
+
+  (compareAndSet [this old-value new-value]
+    (compare-and-set! atom old-value new-value))
+
+  (reset [this new-value]
+    (reset! atom new-value)))
+
+(defn atom [value]
+  (->Atom (clojure.core/atom value)))
+
+(defmethod depend/current-value ::type [dependency]
+  @(:atom dependency))
+
+(defmethod depend/dependency-added ::type [dependency])
---
+++ test/fungl/dependable_atom_test.clj
@@ -0,0 +1,27 @@
+(ns fungl.dependable-atom-test
+   (:require (fungl [cache :as cache]
+                    [dependable-atom :as dependable-atom]))
+   (:use [clojure.test]))
+
+(deftest cache-test
+  (with-bindings (cache/state-bindings)
+    (let [state-atom (dependable-atom/atom 0)
+          call-count-atom (atom 0)
+          function (fn []
+                     (swap! call-count-atom inc)
+                     @state-atom)]
+      (is (= 0 @call-count-atom))
+
+      (is (= 0 (cache/call! function)))
+      (is (= 1 @call-count-atom))
+
+      (is (= 0 (cache/call! function)))
+      (is (= 1 @call-count-atom))
+
+      (swap! state-atom inc)
+
+      (is (= 1 (cache/call! function)))
+      (is (= 2 @call-count-atom))
+
+      (is (= 1 (cache/call! function)))
+      (is (= 2 @call-count-atom)))))
