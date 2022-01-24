2c09a7e4b2bc57977fd18f46b77150b21d48daf3
Jukka Villstedt <jukka.villstedt@iki.fi>
2022-01-24 06:33:59 +0200

    update dependencies

--- project.clj
+++ project.clj
@@ -2,15 +2,15 @@
   :description "FIXME: write description"
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"} ;; for priority-map
   :resource-paths ["resources"]
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [net.mikera/core.matrix "0.33.2"]
                  [com.taoensso/timbre "4.7.3"]
-                 [org.clojure/data.priority-map "0.0.7"]
-                 [org.clojure/core.async "0.4.474"]
-                 [clj-http "2.2.0"]
+                 [org.clojure/data.priority-map "1.0.0"]
+                 [org.clojure/core.async "1.3.610"]
+                 [clj-http "3.7.0"]
                  [org.clojars.jvillste/jogl-all "2.3.2"]
                  [com.google.guava/guava "20.0"]
-                 [org.clojure/tools.namespace "0.2.11"]
+                 [org.clojure/tools.namespace "1.1.0"]
                  [logga "0.1.0-SNAPSHOT"]
                  [slamhound "1.5.5"]]
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}
