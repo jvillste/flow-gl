a9bdccf8fab9f1a6b1821e6a9a53125249cd8a0a
Jukka Villstedt <jukka.villstedt@iki.fi>
2022-03-25 04:55:28 +0200

    fix layout-test

--- test/fungl/layout_test.clj
+++ test/fungl/layout_test.clj
@@ -4,7 +4,8 @@
             [clojure.test :refer [deftest is]]))
 
 (deftest test-adapt-to-space
-  (is (= '{:local-id :new-root,
+  (is (= '{:type :fungl.layouts/vertical-stack,
+           :local-id :new-root,
            :width 100,
            :height 2147483747,
            :available-width 100,
@@ -24,12 +25,12 @@
              :height 100,
              :available-width 100,
              :available-height 2147483647})}
-         (select-layout-keys (layout/do-layout-for-size {:local-id :a
-                                                         :adapt-to-space (fn [node]
-                                                                           (-> (layouts/vertically-2 {}
-                                                                                                     (dissoc node :adapt-to-space)
-                                                                                                     {:width 100 :height 100 :local-id :b})
-                                                                               (assoc :local-id :new-root
-                                                                                      :available-width (:available-width node)
-                                                                                      :available-height (:available-height node))))}
-                                                        100 100)))))
+         (layout/select-layout-keys (layout/do-layout-for-size {:local-id :a
+                                                                :adapt-to-space (fn [node]
+                                                                                  (-> (layouts/vertically-2 {}
+                                                                                                            (dissoc node :adapt-to-space)
+                                                                                                            {:width 100 :height 100 :local-id :b})
+                                                                                      (assoc :local-id :new-root
+                                                                                             :available-width (:available-width node)
+                                                                                             :available-height (:available-height node))))}
+                                                               100 100)))))
