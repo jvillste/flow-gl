e833713310685ae8d448af75e9840b30e7218d47
Jukka Villstedt <jukka.villstedt@iki.fi>
2022-04-18 06:58:33 +0300

    add callable/callable?

--- src/fungl/callable.clj
+++ src/fungl/callable.clj
@@ -1,4 +1,5 @@
-(ns fungl.callable)
+(ns fungl.callable
+  (:require [clojure.test :refer [deftest is]]))
 
 (defn call [callable & arguments]
   (cond (vector? callable)
@@ -17,3 +18,20 @@
 
         (fn? callable)
         (apply callable arguments)))
+
+(defn callable? [value]
+  (or (fn? value)
+      (and (var? value)
+           (fn? @value))
+      (and (vector? value)
+           (or (fn? (first value))
+               (and (var? (first value))
+                    (fn? @(first value)))))))
+
+(deftest test-callable?
+  (is (callable? inc))
+  (is (callable? #'inc))
+  (is (callable? [inc]))
+  (is (callable? [#'inc]))
+
+  (is (not (callable? [1]))))
