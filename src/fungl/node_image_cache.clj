0cf3b98bf0f7b3d88bf500660c21e47fd79a8622
Jukka Villstedt <jukka.villstedt@iki.fi>
2023-09-25 18:52:43 +0300

    use leaf node tree in node image cache

--- src/fungl/node_image_cache.clj
+++ src/fungl/node_image_cache.clj
@@ -1,7 +1,9 @@
 (ns fungl.node-image-cache
   (:require [fungl.cache :as cache]
             [flow-gl.gui.scene-graph :as scene-graph]
-            [flow-gl.gui.visuals :as visuals]))
+            [flow-gl.gui.visuals :as visuals]
+            [medley.core :as medley]
+            clojure.data))
 
 (def ^:dynamic image-cache)
 
@@ -10,6 +12,12 @@
 
 (defn apply-image-cache [scene-graph]
   (scene-graph/map-nodes (fn [node]
+                           #_(prn 'cached?
+                                  (cache/cached-2? image-cache
+                                                   visuals/render-to-images
+                                                   node)
+                                  (:id node)) ;; TODO: remove me
+
                            (if (cache/cached-2? image-cache
                                                 visuals/render-to-images
                                                 node)
@@ -23,19 +31,45 @@
                                                             visuals/render-to-images
                                                             node)))}))
 
-(defn render-recurring-nodes-to-images [previous-scene-graph scene-graph]
-  ;; (prn ) ;; TODO: remove me
-  ;; (prn 'render-recurring-nodes-to-images) ;; TODO: remove me
+(defn leaf-node-tree [scene-graph]
+  (scene-graph/map-nodes (fn [node]
+                           (if (and (:children node)
+                                    (not (:render node)))
+                             (select-keys node [:x :y :z :width :height :type :id :local-id])
+                             node))
+                         scene-graph))
 
-  (let [previous-nodes (into #{} (scene-graph/enumerate-nodes previous-scene-graph))]
+(defn render-recurring-nodes-to-images [previous-scene-graph scene-graph]
+  ;; (prn)
+  ;; (prn 'render-recurring-nodes-to-images)
+
+  (let [previous-leaf-node-tree (leaf-node-tree previous-scene-graph)
+        current-leaf-node-tree (leaf-node-tree scene-graph)
+        previous-nodes (into #{} (scene-graph/enumerate-nodes previous-leaf-node-tree))]
     (scene-graph/map-nodes (fn [node]
+                             ;; (when-let [previous-node (scene-graph/get-in-path previous-leaf-node-tree
+                             ;;                                                   (scene-graph/id-to-local-id-path (:id node)))]
+                             ;;   (let [new-node (scene-graph/get-in-path current-leaf-node-tree
+                             ;;                                           (scene-graph/id-to-local-id-path (:id node)))
+                             ;;         diff (clojure.data/diff (dissoc previous-node :children)
+                             ;;                                 (dissoc new-node :children))]
+                             ;;     (when (not (nil? (first diff)))
+                             ;;       (println)
+                             ;;       (println "node changed: "
+                             ;;                (:type node)
+                             ;;                (:id node))
+                             ;;       (println (second diff)))))
+
                              (if (contains? previous-nodes
                                             node)
-                               (cache/call-with-cache image-cache
-                                                      visuals/render-to-images
-                                                      (apply-image-cache node))
+                               (let [node (apply-image-cache node)]
+                                 (if (= :rendered-to-images (:type node))
+                                   node
+                                   (cache/call-with-cache image-cache
+                                                          visuals/render-to-images
+                                                          node)))
                                node))
-                           scene-graph
+                           current-leaf-node-tree
                            {:descend? (fn [node]
                                         (not (contains? previous-nodes
                                                         node)))})))
