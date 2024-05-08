a2cb7cc5e765b353cb0059ebce1fd0b064af9957
Jukka Villstedt <jukka.villstedt@iki.fi>
2024-05-08 18:36:02 +0300

    add async-profiler as dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject flow-gl "2"
+(defproject flow-gl "3"
   :description "FIXME: write description"
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"} ;; for priority-map
   :resource-paths ["resources"]
@@ -12,8 +12,9 @@
                  #_[org.clojars.jvillste/jogl-all "2.3.2"]
                  [com.google.guava/guava "20.0"]
                  #_[org.clojure/tools.namespace "1.1.0"]
-                 [logga "0.1.0-SNAPSHOT"]
+                 [logga "1"]
                  [slamhound "1.5.5"]
-                 [org.clojure/test.check "1.1.1"]]
+                 [org.clojure/test.check "1.1.1"]
+                 [com.clojure-goes-fast/clj-async-profiler "1.1.1"]]
   :aliases {"slamhound" ["run" "-m" "slam.hound"]}
   :test-paths ["src" "test"])
