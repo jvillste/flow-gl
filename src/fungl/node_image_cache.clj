aa6d8dc4421c29aee26d4fe7cc7b2c5f0f1bd09c
Jukka Villstedt <jukka.villstedt@iki.fi>
2024-09-20 07:55:01 +0300

    use hierarchical cache in node image cache

--- src/fungl/node_image_cache.clj
+++ src/fungl/node_image_cache.clj
@@ -1,75 +1,68 @@
 (ns fungl.node-image-cache
-  (:require [fungl.cache :as cache]
-            [flow-gl.gui.scene-graph :as scene-graph]
-            [flow-gl.gui.visuals :as visuals]
-            [medley.core :as medley]
-            clojure.data))
+  (:require
+   [flow-gl.gui.scene-graph :as scene-graph]
+   [flow-gl.gui.visuals :as visuals]
+   [fungl.hierarchical-identity-cache :as hierarchical-identity-cache]))
 
-(def ^:dynamic image-cache)
+(def ^:dynamic image-cache-atom)
 
 (defn state-bindings []
-  {#'image-cache (cache/create-state 100)})
+  {#'image-cache-atom (hierarchical-identity-cache/create-cache-atom)})
 
 (defn apply-image-cache [scene-graph]
   (scene-graph/map-nodes (fn [node]
-                           #_(prn 'cached?
-                                  (cache/cached-2? image-cache
-                                                   visuals/render-to-images
-                                                   node)
-                                  (:id node)) ;; TODO: remove me
-
-                           (if (cache/cached-2? image-cache
-                                                visuals/render-to-images
-                                                node)
-                             (cache/call-with-cache image-cache
-                                                    visuals/render-to-images
-                                                    node)
+                           (if (hierarchical-identity-cache/cached-call? image-cache-atom
+                                                                         (:id node)
+                                                                         1
+                                                                         visuals/render-to-images
+                                                                         node)
+                             (hierarchical-identity-cache/call-with-cache image-cache-atom
+                                                                          (:id node)
+                                                                          1
+                                                                          visuals/render-to-images
+                                                                          node)
                              node))
                          scene-graph
                          {:descend? (fn [node]
-                                      (not (cache/cached-2? image-cache
-                                                            visuals/render-to-images
-                                                            node)))}))
+                                      (not (hierarchical-identity-cache/cached-call? image-cache-atom
+                                                                                     (:id node)
+                                                                                     1
+                                                                                     visuals/render-to-images
+                                                                                     node)))}))
 
-(defn leaf-node-tree [scene-graph]
-  (scene-graph/map-nodes (fn [node]
-                           (if (and (:children node)
-                                    (not (:render node)))
-                             (select-keys node [:x :y :z :width :height :type :id :local-id])
-                             node))
-                         scene-graph))
+(defn apply-cache-and-render-to-images [scene-graph]
+  (-> scene-graph
+      (apply-image-cache)
+      (visuals/render-to-images)))
 
 (defn render-recurring-nodes-to-images [previous-scene-graph scene-graph]
-  ;; (prn)
-  ;; (prn 'render-recurring-nodes-to-images)
+  (if (nil? previous-scene-graph)
+    scene-graph
+    (hierarchical-identity-cache/with-cache-cleanup image-cache-atom
+      (if (identical? previous-scene-graph scene-graph)
+        (hierarchical-identity-cache/call-with-cache image-cache-atom
+                                                     (:id scene-graph)
+                                                     1
+                                                     apply-cache-and-render-to-images
+                                                     scene-graph)
 
-  (let [previous-leaf-node-tree (leaf-node-tree previous-scene-graph)
-        current-leaf-node-tree (leaf-node-tree scene-graph)
-        previous-nodes (into #{} (scene-graph/enumerate-nodes previous-leaf-node-tree))]
-    (scene-graph/map-nodes (fn [node]
-                             ;; (when-let [previous-node (scene-graph/get-in-path previous-leaf-node-tree
-                             ;;                                                   (scene-graph/id-to-local-id-path (:id node)))]
-                             ;;   (let [new-node (scene-graph/get-in-path current-leaf-node-tree
-                             ;;                                           (scene-graph/id-to-local-id-path (:id node)))
-                             ;;         diff (clojure.data/diff (dissoc previous-node :children)
-                             ;;                                 (dissoc new-node :children))]
-                             ;;     (when (not (nil? (first diff)))
-                             ;;       (println)
-                             ;;       (println "node changed: "
-                             ;;                (:type node)
-                             ;;                (:id node))
-                             ;;       (println (second diff)))))
-
-                             (if (contains? previous-nodes
-                                            node)
-                               (let [node (apply-image-cache node)]
-                                 (if (= :rendered-to-images (:type node))
-                                   node
-                                   (cache/call-with-cache image-cache
-                                                          visuals/render-to-images
-                                                          node)))
-                               node))
-                           current-leaf-node-tree
-                           {:descend? (fn [node]
-                                        (not (contains? previous-nodes
-                                                        node)))})))
+        (if (contains? scene-graph :children)
+          (update scene-graph
+                  :children
+                  (fn [children]
+                    (mapv (fn [[previous-child child]]
+                            (if (and (some? previous-child)
+                                     (or (identical? previous-child child)
+                                         (and (= (:type previous-child)
+                                                 (:type child))
+                                              (= (:local-id previous-child)
+                                                 (:local-id child)))))
+                              (render-recurring-nodes-to-images previous-child child)
+                              child))
+                          (partition 2 (interleave (concat (:children previous-scene-graph)
+                                                           (repeat (max 0
+                                                                        (- (count children)
+                                                                           (count (:children previous-scene-graph))))
+                                                                   nil))
+                                                   children)))))
+          scene-graph)))))
