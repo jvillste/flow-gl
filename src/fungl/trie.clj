8272c9d077348df5e86ad3cfeb22ff06913f13d3
Jukka Villstedt <jukka.villstedt@iki.fi>
2025-02-07 11:07:05 +0200

    add trie/dissoc-in-trie

--- src/fungl/trie.clj
+++ src/fungl/trie.clj
@@ -27,29 +27,87 @@
              (update-in-trie [:a] (constantly 1))
              (update-in-trie [:a] inc)))))
 
-(defn assoc-trie [trie path value]
+(defn assoc-in-trie [trie path value]
   (update-in-trie trie path (constantly value)))
 
-(deftest test-assoc-trie
+(deftest test-assoc-in-trie
   (is (= {::value :value}
-         (assoc-trie (create-trie) [] :value)))
+         (assoc-in-trie (create-trie) [] :value)))
 
   (is (= {:a {::value :value}}
-         (assoc-trie (create-trie) [:a]  :value)))
+         (assoc-in-trie (create-trie) [:a]  :value)))
 
   (is (= {:a {:b {::value :value}
               ::value :value}}
          (-> (create-trie)
-             (assoc-trie [:a] :value)
-             (assoc-trie [:a :b] :value))))
+             (assoc-in-trie [:a] :value)
+             (assoc-in-trie [:a :b] :value))))
 
   (is (= {:a {:b {::value :value}
               :c {::value :value}
               ::value :value}}
          (-> (create-trie)
-             (assoc-trie [:a] :value)
-             (assoc-trie [:a :b] :value)
-             (assoc-trie [:a :c] :value)))))
+             (assoc-in-trie [:a] :value)
+             (assoc-in-trie [:a :b] :value)
+             (assoc-in-trie [:a :c] :value)))))
+
+(defn dissoc-in-trie [trie path]
+  (if (= 1 (count path))
+    (if (contains? trie (first path))
+      (if (= [::value] (keys (get trie (first path))))
+        (dissoc trie (first path))
+        (update trie (first path) dissoc ::value))
+      trie)
+    (if (contains? trie (first path))
+      (let [new-trie (update trie
+                             (first path)
+                             (fn [node]
+                               (dissoc-in-trie
+                                node
+                                (rest path))))]
+        (if (empty? (get new-trie (first path)))
+          (dissoc new-trie (first path))
+          new-trie))
+      trie)))
+
+(deftest test-dissoc-in-trie
+  (is (= {}
+         (-> (create-trie)
+             (assoc-in-trie [1] :foo)
+             (dissoc-in-trie [1]))))
+
+  (is (= {1 {1 {::value :foo}}}
+         (-> (create-trie)
+             (assoc-in-trie [1 1] :foo)
+             (dissoc-in-trie [1]))))
+
+  (is (= {1 {1 {::value :foo}}}
+         (-> (create-trie)
+             (assoc-in-trie [1 1] :foo)
+             (dissoc-in-trie [2]))))
+
+  (is (= {1 {1 {::value :foo}}}
+         (-> (create-trie)
+             (assoc-in-trie [1 1] :foo)
+             (dissoc-in-trie [1 2]))))
+
+  (is (= {}
+         (-> (create-trie)
+             (assoc-in-trie [1 1] :foo)
+             (dissoc-in-trie [1 1]))))
+
+  (is (= {1 {1 {::value :foo}}}
+         (-> (create-trie)
+             (assoc-in-trie [1 1] :foo)
+             (assoc-in-trie [1 2] :bar)
+             (dissoc-in-trie [1 2]))))
+
+  (is (= {}
+         (-> (create-trie)
+             (assoc-in-trie [1 1] :foo)
+             (assoc-in-trie [1 2] :bar)
+             (dissoc-in-trie [1 2])
+             (dissoc-in-trie [1 1])))))
 
 (defn get-in-trie [trie path]
   (::value (get-in trie path)))
@@ -57,12 +115,12 @@
 (deftest test-get-in-trie
   (is (= :value
          (-> (create-trie)
-             (assoc-trie [:a :b] :value)
+             (assoc-in-trie [:a :b] :value)
              (get-in-trie [:a :b]))))
 
   (is (= nil
          (-> (create-trie)
-             (assoc-trie [:a :b] :value)
+             (assoc-in-trie [:a :b] :value)
              (get-in-trie [:a])))))
 
 (defn last-value-on-path [trie path]
@@ -76,40 +134,40 @@
       (::value trie))))
 
 (deftest test-last-value-on-path
-  (is (= :value (last-value-on-path (assoc-trie (create-trie) [] :value)
+  (is (= :value (last-value-on-path (assoc-in-trie (create-trie) [] :value)
                                     [])))
 
-  (is (= :value (last-value-on-path (assoc-trie (create-trie) [:a] :value)
+  (is (= :value (last-value-on-path (assoc-in-trie (create-trie) [:a] :value)
                                     [:a])))
 
-  (is (= :value (last-value-on-path (assoc-trie (create-trie) [:a :b] :value)
+  (is (= :value (last-value-on-path (assoc-in-trie (create-trie) [:a :b] :value)
                                     [:a :b])))
 
-  (is (= :value (last-value-on-path (assoc-trie (create-trie) [:a] :value)
+  (is (= :value (last-value-on-path (assoc-in-trie (create-trie) [:a] :value)
                                     [:a :b])))
 
   (is (= :value-1 (last-value-on-path (-> (create-trie)
-                                          (assoc-trie [:a :b] :value-1)
-                                          (assoc-trie [:a :c] :value-2))
+                                          (assoc-in-trie [:a :b] :value-1)
+                                          (assoc-in-trie [:a :c] :value-2))
                                       [:a :b])))
 
   (is (= :value-2 (last-value-on-path (-> (create-trie)
-                                          (assoc-trie [:a :b] :value-1)
-                                          (assoc-trie [:a :b :c] :value-2))
+                                          (assoc-in-trie [:a :b] :value-1)
+                                          (assoc-in-trie [:a :b :c] :value-2))
                                       [:a :b :c])))
 
   (is (= :value-1 (last-value-on-path (-> (create-trie)
-                                          (assoc-trie [:a :b] :value-1)
-                                          (assoc-trie [:a :b :c] :value-2))
+                                          (assoc-in-trie [:a :b] :value-1)
+                                          (assoc-in-trie [:a :b :c] :value-2))
                                       [:a :b])))
 
-  (is (nil? (last-value-on-path (assoc-trie (create-trie) [:a] :value)
+  (is (nil? (last-value-on-path (assoc-in-trie (create-trie) [:a] :value)
                                 [])))
 
-  (is (nil? (last-value-on-path (assoc-trie (create-trie) [:a :b :c] :value)
+  (is (nil? (last-value-on-path (assoc-in-trie (create-trie) [:a :b :c] :value)
                                 [:a :b])))
 
-  (is (nil? (last-value-on-path (assoc-trie (create-trie) [:a] :value)
+  (is (nil? (last-value-on-path (assoc-in-trie (create-trie) [:a] :value)
                                 [:b]))))
 
 
@@ -126,5 +184,5 @@
 (deftest test-value-count
   (is (= 2
          (value-count (-> (create-trie)
-                          (assoc-trie [:a :b] :value-1)
-                          (assoc-trie [:a :b :c] :value-2))))))
+                          (assoc-in-trie [:a :b] :value-1)
+                          (assoc-in-trie [:a :b :c] :value-2))))))
