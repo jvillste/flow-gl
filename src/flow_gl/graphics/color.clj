8e2b44270108a4dd957c6520ad0cbb75a4c5768d
Jukka Villstedt <jukka.villstedt@iki.fi>
2025-09-24 18:03:22 +0300

    fix float color value handling

--- src/flow_gl/graphics/color.clj
+++ src/flow_gl/graphics/color.clj
@@ -1,7 +1,9 @@
 (ns flow-gl.graphics.color)
 
 (defn convert-color-channel-values-to-floats [color-channel-values]
-  (map (fn [color-channel-value] (if (integer? color-channel-value)
-                                   (float (/ color-channel-value 255))
-                                   (float color-channel-value)))
+  (map (fn [color-channel-value]
+         (if (or (integer? color-channel-value)
+                 (< 1 color-channel-value))
+           (float (/ color-channel-value 255))
+           (float color-channel-value)))
        color-channel-values))
